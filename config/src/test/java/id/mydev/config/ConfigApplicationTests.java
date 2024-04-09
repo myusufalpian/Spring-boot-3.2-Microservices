bf2f6e7cb937b6b4f445bdc9c80e2eacf156ddd9
Muhammad Yusuf Alpian <llawliet1945@gmail.com>
2024-04-09 09:31:10 +0700

    initial commit

---
+++ config/src/test/java/id/mydev/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package id.mydev.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
